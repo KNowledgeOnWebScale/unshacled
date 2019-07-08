a5ed51d096de1c1ecb819e4a7711c7d53ab919f5
Femke <femke.bruckmann@ugent.be>
2019-07-08 11:03:28 +0200

    Chore: create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
+    <nav-bar></nav-bar>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar.vue";
 
 export default {
-  name: "app",
+  name: "App",
   components: {
-    HelloWorld
+    NavBar
   }
 };
 </script>
@@ -23,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,29 @@
-<template />
+<template>
+  <sui-menu attached="top" inverted="true">
+    <sui-dropdown-menu>
+      <sui-dropdown item icon="file alternate" simple>
+        <sui-dropdown-menu>
+          <sui-dropdown-item>Import</sui-dropdown-item>
+          <sui-dropdown-item>Export</sui-dropdown-item>
+          <sui-dropdown-divider></sui-dropdown-divider>
+          <sui-dropdown-item>Load Example</sui-dropdown-item>
+        </sui-dropdown-menu>
+      </sui-dropdown>
+    </sui-dropdown-menu>
+    <sui-menu-item icon="add">Shape</sui-menu-item>
+    <sui-menu-menu position="right">
+      <sui-menu-item icon="user"></sui-menu-item>
+    </sui-menu-menu>
+  </sui-menu>
+</template>
 
 <script>
+import SuiDropdown from "semantic-ui-vue/dist/commonjs/modules/Dropdown/Dropdown";
+import SuiDropdownDivider from "semantic-ui-vue/dist/commonjs/modules/Dropdown/DropdownDivider";
+
 export default {
-  name: "NavBar"
+  name: "NavBar",
+  components: { SuiDropdownDivider, SuiDropdown }
 };
 </script>
 
