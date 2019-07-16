58d7685a0e92b32ea73b8b42c0672363af32c176
Pieter-Jan Vandenberghe <pieterjan.vandenberghe@outlook.com>
2019-07-16 20:27:52 +0200

    Include optional arguments in traverse function

--- src/util/traverse.js
+++ src/util/traverse.js
@@ -1,13 +1,14 @@
 /**
- *
+ * TODO
  * @param o
  * @param func
+ * @param args Arguments passed on to the function NOTE: after index and object
  */
-export default function traverse(o, func) {
+export default function traverse(o, func, ...args) {
   for (const i in o) {
-    func.apply(this, [i, o[i]]);
+    func.apply(this, [i, o[i], ...args]);
     if (o[i] !== null && typeof o[i] === "object") {
-      traverse(o[i], func);
+      traverse(o[i], func, ...args);
     }
   }
 }
