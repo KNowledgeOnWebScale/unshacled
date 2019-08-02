fcd44e011a8b7df08529e8734c008470b936462b
Femke <femke.bruckmann@ugent.be>
2019-08-02 15:45:30 +0200

    Canvas panning

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,5 +1,12 @@
 <template>
-  <v-stage ref="stage" :config="configKonva" @wheel="scroll">
+  <v-stage
+    ref="stage"
+    :config="configKonva"
+    @wheel="scroll"
+    @mousedown="startPan"
+    @mousemove="pan"
+    @mouseup="stopPan"
+  >
     <v-layer>
       <div v-for="(obj, key) in this.$store.getters.nodeShapes" :key="key">
         <shape :id="key" :node-shape="true"></shape>
@@ -21,6 +28,8 @@ export default {
   data() {
     const marginTop = 40;
     return {
+      panning: false,
+      previousPosition: undefined,
       marginTop, // Provide space for the NavBar
       configKonva: {
         width: window.innerWidth,
@@ -44,6 +53,48 @@ export default {
       this.configKonva.height = window.innerHeight - this.marginTop;
       this.configKonva.width = window.innerWidth;
       this.$nextTick(() => stage.draw()); // Resize on the next tick
+    },
+
+    /**
+     * Determine the current position and start panning.
+     */
+    startPan() {
+      this.panning = true;
+      this.previousPosition = this.$refs.stage.getNode().getPointerPosition();
+    },
+
+    /**
+     * Stop panning.
+     */
+    stopPan() {
+      this.panning = false;
+      this.previousPosition = undefined;
+    },
+
+    /**
+     * Move the whole stage while the user is dragging the canvas.
+     */
+    pan() {
+      if (this.panning) {
+        const stage = this.$refs.stage.getNode();
+        // Determine the current position and the difference with the previous one.
+        const newPosition = stage.getPointerPosition();
+        const delta = {
+          x: newPosition.x - this.previousPosition.x,
+          y: newPosition.y - this.previousPosition.y
+        };
+
+        // Calculate the new position of the stage.
+        const sp = {
+          x: stage.x() + delta.x,
+          y: stage.y() + delta.y
+        };
+
+        // Set the stage to this position and draw the contents.
+        stage.position(sp);
+        stage.batchDraw();
+        this.previousPosition = newPosition;
+      }
     },
 
     /**
