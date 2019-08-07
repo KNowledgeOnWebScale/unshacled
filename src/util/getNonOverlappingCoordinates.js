63cac2ac2086f3eeea007b33d750e4c652da7387
Femke <femke.bruckmann@ugent.be>
2019-08-07 17:16:11 +0200

    Improve overlapping coordinates

--- src/util/getNonOverlappingCoordinates.js
+++ src/util/getNonOverlappingCoordinates.js
@@ -1,5 +1,5 @@
 import { values } from "ramda";
-import { HEIGHT, WIDTH } from "./konvaConfigs";
+import { WIDTH } from "./konvaConfigs";
 
 /** @returns {x: number, y: number} the coordinates for a newly added shapeID */
 export default ({ coordinates, bottomYs, bottoms }) => {
@@ -7,30 +7,41 @@ export default ({ coordinates, bottomYs, bottoms }) => {
   const MARGIN = 16;
   const { innerWidth } = window;
 
-  // Maximum vertical position of the existing shapes.
-  const yMax = Math.max(...[0, ...Object.values(bottomYs)]);
+  // Get the y value of the bottom row.
+  const yBottomRow = Math.max(
+    ...[0, ...Object.keys(coordinates).map(key => coordinates[key].y)]
+  );
 
-  // Get the lowest shapes.
-  const lowest = Object.keys(bottomYs).filter(key => bottomYs[key] >= yMax);
+  // Get the shapes on the bottom row.
+  const bottomRow = Object.keys(coordinates).filter(
+    key => coordinates[key].y === yBottomRow
+  );
 
-  // Determine which shape is the most to the right.
-  let xMax = 0;
-  let refY = 0;
-  let refKey;
-  lowest.map(key => {
-    if (coordinates[key].x >= xMax) {
-      xMax = coordinates[key].x;
-      refY = coordinates[key].y;
-      refKey = key;
-    }
-  });
+  // Get the deepest y value on the bottom row.
+  const deepestY = Math.max(...bottomRow.map(key => bottomYs[key]));
+  // Get the shape that has the deepest y value on the bottom row.
+  const deepest = bottomRow.filter(key => bottomYs[key] >= deepestY);
+
+  // Get the highest x value on the bottom row.
+  const rightestX = Math.max(
+    ...[0, ...bottomRow.map(key => coordinates[key].x)]
+  );
 
   // If negative, add the shapeID to the right; if positive, a bit lower to the left.
-  const criterion = xMax + 2 * (MARGIN + WIDTH) > innerWidth;
-  const x =
-    MARGIN + (criterion ? 0 : xMax + (values(coordinates).length ? WIDTH : 0));
-  const y =
-    refY + (criterion ? MARGIN + (refKey ? bottoms[refKey] : 0) : 0) || MARGIN;
+  const newLine = rightestX + 2 * (MARGIN + WIDTH) > innerWidth;
+
+  // Calculate x coordinate
+  let x = MARGIN;
+  if (!newLine) x += rightestX + (values(coordinates).length ? WIDTH : 0);
+
+  // Calculate y coordinate
+  let y = 0;
+  if (deepest.length > 0) y += coordinates[deepest[0]].y; // On the same row as the deepest shape.
+  if (newLine) {
+    y += MARGIN;
+    if (deepest.length > 0) y += deepestY;
+  }
+  if (y === 0) y = MARGIN;
 
   return { x, y };
 };
